1cc65207aabc83d0267ef69509a9637242abd715
Git <git@what.cd>
2012-01-12 08:00:37 +0000

    Empty commit

--- sections/schedule/index.php
+++ sections/schedule/index.php
@@ -2,7 +2,7 @@
 set_time_limit(50000);
 ob_end_flush();
 gc_enable();
-//TODO: make it awesome, make it flexible
+//TODO: make it awesome, make it flexible!
 //INSERT INTO users_geodistribution (Code, Users) SELECT g.Code, COUNT(u.ID) AS Users FROM geoip_country AS g JOIN users_main AS u ON INET_ATON(u.IP) BETWEEN g.StartIP AND g.EndIP WHERE u.Enabled='1' GROUP BY g.Code ORDER BY Users DESC
 /*************************************************************************\
 //--------------Schedule page -------------------------------------------//
