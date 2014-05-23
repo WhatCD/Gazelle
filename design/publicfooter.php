337bb97c6a3930cf793ab663d4c80a201c322696
Git <git@what.cd>
2014-05-23 08:00:28 +0000

    Empty commit

--- design/publicfooter.php
+++ design/publicfooter.php
@@ -2,7 +2,7 @@
 	</tr>
 </table>
 <div id="foot">
-	<span><a href="#"><?=SITE_NAME?></a> | <a href="http://what-network.net">What-Network</a> | <a href="https://what.cd/gazelle/">Project Gazelle</a></span>
+	<span><a href="#"><?=SITE_NAME?></a> | <a href="https://what.cd/gazelle/">Project Gazelle</a></span>
 </div>
 </body>
 </html>
