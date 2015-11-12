411a01201586b60bbca954cb8f661a2ce9cd6ef9
Git <git@what.cd>
2015-11-12 08:00:27 +0000

    Empty commit

--- sections/torrents/reseed.php
+++ sections/torrents/reseed.php
@@ -89,7 +89,7 @@ View::show_header();
 		<h2>Successfully sent re-seed request</h2>
 	</div>
 	<div class="box pad thin">
-		<p>Successfully sent re-seed request for torrent <a href="torrents.php?id=<?=$GroupID?>&amp;torrentid=<?=$TorrentID?>"><?=display_str($Name)?></a> to <?=$NumUsers?> user<?=$NumUsers === 1 ? '' : 's';?>.</p>
+		<p style="color: black;">Successfully sent re-seed request for torrent <a href="torrents.php?id=<?=$GroupID?>&amp;torrentid=<?=$TorrentID?>"><?=display_str($Name)?></a> to <?=$NumUsers?> user<?=$NumUsers === 1 ? '' : 's';?>.</p>
 	</div>
 </div>
 <?
