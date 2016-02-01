85fad9337ff83ef2c9cea44a9f829cf5800028e5
Git <git@what.cd>
2016-02-01 08:00:30 +0000

    Empty commit

--- design/views/generic/reply/staffpm.php
+++ design/views/generic/reply/staffpm.php
@@ -13,8 +13,10 @@
 
 				<strong>Send to: </strong>
 				<select name="level">
+<?				if (!isset(G::$LoggedUser['LockedAccount'])) { ?>
 					<option value="0" selected="selected">First Line Support</option>
 					<option value="650">Forum Moderators</option>
+<?				} ?>
 					<option value="700">Staff</option>
 				</select>
 
